1b94591d1e5ae52aff348d19af76ba1b5ce1fc21
hamax <jure.ham@zemanta.com>
2012-07-09 21:03:29 +0200

    basice setup.py file

---
+++ django_probe/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='django_probe',
+      version='0.1',
+      description='Django Probe',
+      author='Dusan Omercevic',
+      packages=['django_probe'],
+      package_dir = {'django_probe':'.'},
+     )
